afd5042f8fa0141912d2687dcc54f57ce4c065f9
China131 <JHSilkTree@foxmail.com>
2017-11-29 14:03:45 +0800

    config

---
+++ src/main/java/cn/javadevelop/jdt/controller/TestController.java
@@ -0,0 +1,14 @@
+package cn.javadevelop.jdt.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by jianhao on 2017/11/29.
+ */
+@Controller
+public class TestController {
+
+
+}
+
+
