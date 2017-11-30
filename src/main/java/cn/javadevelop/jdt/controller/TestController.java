9c78ec38d16859e511c78dc86b2d981d00c8200c
China131 <JHSilkTree@foxmail.com>
2017-11-30 14:35:06 +0800

    test api and db

--- src/main/java/cn/javadevelop/jdt/controller/TestController.java
+++ src/main/java/cn/javadevelop/jdt/controller/TestController.java
@@ -1,13 +1,31 @@
 package cn.javadevelop.jdt.controller;
 
+import cn.javadevelop.jdt.mapper.read.TestReadMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by jianhao on 2017/11/29.
  */
+@RequestMapping(value = "/api")
 @Controller
 public class TestController {
 
+    @Autowired
+    private TestReadMapper testReadMapper;
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    @ResponseBody
+    public String test(){
+        List<Map> list = testReadMapper.selectTest();
+
+        return list.toString();
+    }
 
 }
 
---
+++ src/main/java/cn/javadevelop/jdt/mapper/read/TestReadMapper.java
@@ -0,0 +1,11 @@
+package cn.javadevelop.jdt.mapper.read;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by jianhao on 2017/11/30.
+ */
+public interface TestReadMapper {
+    public List<String> selectTest();
+}
