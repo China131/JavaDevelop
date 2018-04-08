c6e795f12baa92454944ba421e7e34a8eb0579ba
jianhao <jhsilktree@foxmail.com>
2018-04-08 18:40:31 +0800

    save work

--- src/main/java/com/learn/ssm/chapter3/main/Chapter3Main.java
+++ src/main/java/com/learn/ssm/chapter3/main/Chapter3Main.java
@@ -4,7 +4,6 @@ package com.learn.ssm.chapter3.main;
 import com.learn.ssm.chapter3.mapper.RoleMapper;
 import com.learn.ssm.chapter3.pojo.Role;
 import com.learn.ssm.chapter3.utils.SqlSessionFactoryUtils;
-import com.oracle.tools.packager.Log;
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 
@@ -19,7 +18,6 @@ public class Chapter3Main {
             sqlSession = SqlSessionFactoryUtils.openSqlSession();
             RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
             Role role = roleMapper.getRole(1L);
-            Log.info(role.getRoleName());
             roleMapper.findRoles("43");
         }finally {
             if (sqlSession != null){
